333a2a2fad1010ed5459d6efbe0b009732d563e1
Leonardo Banderali <lbanderali@yahoo.com>
2015-08-30 12:22:36 -0600

    Change semantics of the code.
    Make the `Token` class and `BasicRule` class template be more
    specific as to what they represent.

--- include/ogla/token.hpp
+++ include/ogla/token.hpp
@@ -3,7 +3,7 @@ Project: OGLA
 File: token.hpp
 Author: Leonardo Banderali
 Created: July 7, 2015
-Last Modified: August 26, 2015
+Last Modified: August 30, 2015
 
 Description:
     A `Token` is a unit of analyzed text and is identified using a `Rule`.  These form the basic building blocks of the
@@ -27,8 +27,8 @@ namespace ogla {
 
 //~forward declarations and function prototypes~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-template <typename NextRulesPointer> class BasicRule;   // type for describing a rule used to identify a token
-class Token;                                            // type representing a token in analyzed text
+template <typename NextStateType> class BasicRule;  // type for describing a rule used to identify a token
+class Token;                                        // type representing a token in analyzed text
 using TokenList = std::vector<Token>;
 struct TokenRulePair;
 
@@ -37,40 +37,40 @@ struct TokenRulePair;
 //~class templates~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 /*
-A class template for describing a rule used to identify a token (tokenization rule).
+A class template for describing a rule used to find a token (tokenization rule).
 */
-template <typename NextRulesPointer>
+template <typename NextStateType>
 class BasicRule {
     public:
-        BasicRule(NextRulesPointer _nextRules) : nextRules{_nextRules} {}
-        BasicRule(const std::string& _name, const std::string& _regex, NextRulesPointer _nextRules)
-            : ruleName{_name}, rgx{_regex}, nextRules{_nextRules} {}
+        BasicRule(NextStateType _nextState) : nextState{_nextState} {}
+        BasicRule(const std::string& _name, const std::string& _regex, NextStateType _nextState)
+            : ruleName{_name}, rgx{_regex}, nextState{_nextState} {}
 
         std::string name() const;
 
         std::regex regex() const;
 
-        NextRulesPointer get_nextRules() const;
+        NextStateType next() const;
 
     private:
         std::string ruleName;
         std::regex rgx;             // holds the regular expression (regex) used to indentify the token
-        NextRulesPointer nextRules; // points to (but does not own) the next rules to be used for tokenization
+        NextStateType nextState;    // points to (but does not own) the next rules to be used for tokenization
 };
 
-template <typename NextRulesPointer>
-std::string BasicRule<NextRulesPointer>::name() const {
+template <typename NextStateType>
+std::string BasicRule<NextStateType>::name() const {
     return ruleName;
 }
 
-template <typename NextRulesPointer>
-std::regex BasicRule<NextRulesPointer>::regex() const {
+template <typename NextStateType>
+std::regex BasicRule<NextStateType>::regex() const {
     return rgx;
 }
 
-template <typename NextRulesPointer>
-NextRulesPointer BasicRule<NextRulesPointer>::get_nextRules() const {
-    return nextRules;
+template <typename NextStateType>
+NextStateType BasicRule<NextStateType>::next() const {
+    return nextState;
 }
 
 
@@ -79,9 +79,11 @@ NextRulesPointer BasicRule<NextRulesPointer>::get_nextRules() const {
 
 class Token {
     public:
-        Token() : offset{0} {}
-        Token(const std::string& _ruleName, const std::smatch& _match, int _offset = 0)
-            :ruleName{_ruleName}, match{_match}, offset{_offset} {}
+        Token() = default;
+        Token(const std::string& _ruleName, const std::smatch& _match, int _pos = -1)
+            :ruleName{_ruleName}, match{_match}, pos{_pos} {}
+
+        bool empty() const;
 
         std::string name() const;
 
@@ -97,8 +99,8 @@ class Token {
 
     private:
         std::string ruleName;
-        std::smatch match;  // holds the lexem matched associated with the token
-        int offset;         // holds the offset for the token position
+        std::smatch match;  // the matched lexeme associated with the token
+        int pos = -1;       // the assigned position of the token in the text (-1 is "no/don't care position")
 };
 
 }   // `ogla` namepsace
--- src/token.cpp
+++ src/token.cpp
@@ -3,7 +3,7 @@ Project: OGLA
 File: token.cpp
 Author: Leonardo Banderali
 Created: July 7, 2015
-Last Modified: August 26, 2015
+Last Modified: August 30, 2015
 
 Description:
     A `Token` is a unit of analyzed text and is identified using a `Rule`.  These form the basic building blocks of the
@@ -22,15 +22,16 @@ Distributed under the Boost Software License, Version 1.0.
 
 //~`Token` public functions~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+bool ogla::Token::empty() const {
+    return match.empty();
+}
+
 std::string ogla::Token::name() const {
     return ruleName;
 }
 
 int ogla::Token::position() const {
-    if (match.empty())
-        return -1;
-    else
-        return match.position() + offset;
+    return pos;
 }
 
 int ogla::Token::length() const {
@@ -50,7 +51,7 @@ std::string ogla::Token::lexeme() const {
 bool ogla::Token::operator==(const Token& other) const {
     return ruleName == other.ruleName &&
            match == other.match &&
-           offset == other.offset;
+           pos == other.pos;
 }
 
 bool ogla::Token::operator!=(const Token& other) const {
