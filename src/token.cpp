d624cd0bd345439989dbb70fc65458800fcf499e
Leonardo Banderali <lbanderali@yahoo.com>
2015-09-15 17:26:42 -0600

    Make the token type a template parameter.

--- src/token.cpp
+++ src/token.cpp
@@ -3,7 +3,7 @@ Project: OGLA
 File: token.cpp
 Author: Leonardo Banderali
 Created: July 7, 2015
-Last Modified: August 31, 2015
+Last Modified: September 15, 2015
 
 Description:
     A `Token` is a unit of analyzed text and is identified using a `Rule`.  These form the basic building blocks of the
@@ -17,46 +17,3 @@ Distributed under the Boost Software License, Version 1.0.
 
 // library header
 #include "token.hpp"
-
-
-
-//~`Token` public functions~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-/*
-returns true if the token is the result of an empty match (search result is empty)
-*/
-bool ogla::Token::empty() const {
-    return match.empty();
-}
-
-/*
-returns the type of the token
-*/
-std::string ogla::Token::type() const {
-    return tokenType;
-}
-
-/*
-returns the specifed position of the token within the text searched (-1 is "no/don't care position")
-*/
-int ogla::Token::position() const {
-    return pos;
-}
-
-/*
-returns the lexeme of this token
-*/
-std::string ogla::Token::lexeme() const {
-    if (match.empty())
-        return std::string();
-    else
-        return match.str();
-}
-
-bool ogla::Token::operator==(const Token& other) const {
-    return tokenType == other.tokenType && match == other.match && pos == other.pos;
-}
-
-bool ogla::Token::operator!=(const Token& other) const {
-    return !(*this == other);
-}
